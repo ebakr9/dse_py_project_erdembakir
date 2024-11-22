84da717638500cc70e4a6147ce854ac76f7d857e
unknown <erdem.bakir@tedu.edu.tr>
2024-11-22 12:10:12 +0100

    Added type feature for the condition in mainapp

--- dse_project/config.py
+++ dse_project/config.py
@@ -3,25 +3,29 @@ DATASETS = {
     'major_cities_data': {
         'path': 'C:/Users/Erdem/Desktop/UNIMI/dse_project/data/GlobalLandTemperaturesByMajorCity.csv',
         'description': 'Temperature data for major cities worldwide',
-        'needs_preprocess':True
+        'needs_preprocess':True,
+        'type':'city'
     },
 
     'country_data': {
         'path': 'C:/Users/Erdem/Desktop/UNIMI/dse_project/data/GlobalLandTemperaturesByCountry.csv',
         'description': 'Temperature data for countries in the world',
-        'needs_preprocess':False
+        'needs_preprocess':False,
+        'type':'country'
     },
 
     'all_cities_data':{
         'path':'C:/Users/Erdem/Desktop/UNIMI/dse_project/data/GlobalLandTemperaturesByCity.csv',
         'description': 'Temperature data for more cities in the world.',
-        'needs_preprocess':True
+        'needs_preprocess':True,
+        'type':'city'
     },
 
     'state_data':{
         'path':'C:/Users/Erdem/Desktop/UNIMI/dse_project/data/GlobalLandTemperaturesByState.csv',
         'description':' Temperature data for staten is the world',
-        'needs_preprocess':False
+        'needs_preprocess':False,
+        'type':'state'
     }
 }
 
